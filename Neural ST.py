a787db40d93ae89e37f7d74d35d00b96c4905d2f
PrajwalSood <prajwalsood@gmail.com>
2021-04-05 16:48:40 +0530

    Added Images and Defined Imports

---
+++ Neural ST.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  5 16:18:42 2021
+
+@author: prajw
+"""
+
+import os
+import tensorflow as tf
+
+os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
+
+import IPython.display as display
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (12, 12)
+mpl.rcParams['axes.grid'] = False
+
+import numpy as np
+import PIL.Image
+import time
+import functools
+
+def tensor_to_image(tensor):
+  tensor = tensor*255
+  tensor = np.array(tensor, dtype=np.uint8)
+  if np.ndim(tensor)>3:
+    assert tensor.shape[0] == 1
+    tensor = tensor[0]
+  return PIL.Image.fromarray(tensor)
+
+image_path = ''
+style_path = ''
